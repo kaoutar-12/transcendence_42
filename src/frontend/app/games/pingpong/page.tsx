b37c2ce21dc4719aa691a2558f5ac554d217e6f1
yrhiba <yahyarhiba@gmail.com>
2025-01-05 15:34:20 +0100

    Modify the pingpong game page to use the new game component
    	modified:   src/frontend/app/games/pingpong/page.tsx
    	modified:   src/frontend/styles/game.css

--- src/frontend/app/games/pingpong/page.tsx
+++ src/frontend/app/games/pingpong/page.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import "@/styles/game.css"
+import GameLinkItem from '@/components/GameLinkItem';
 
 type Props = {}
 
 const PingPong = (props: Props) => {
   return (
-    <div>
-      Ping Pong Game
+    <div className="games-container">
+      <div className="games-header">
+        <h1>Tic Tac Toe | Chosse a Game Mode </h1>
+      </div>
+      <div className="games-list">
+        <GameLinkItem href="/games/pingpong/vs-random" title="VS Random" image="/vs-random.png" />
+        <GameLinkItem href="/games/pingpong/vs-ia" title="VS IA" image="/vs-ia.png" />
+      </div>
     </div>
   )
 }
