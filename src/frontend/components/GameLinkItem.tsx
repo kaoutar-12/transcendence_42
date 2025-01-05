ca97da52b7c18dc7262938a91bfe2f1102d96564
yrhiba <yahyarhiba@gmail.com>
2025-01-05 15:28:35 +0100

    Add GameLinkItem component
    	new file:   src/frontend/components/GameLinkItem.tsx
    	new file:   src/frontend/styles/game-link-item.css

---
+++ src/frontend/components/GameLinkItem.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import "@/styles/game-link-item.css"
+
+type Props = {
+    title: string;
+    image: string;
+    href: string;
+};
+
+const GameLinkItem = (props: Props) => {
+  return (
+    <div className='game-item'>
+        <Link className="link" href={props.href}>
+        <div className='title'>{props.title}</div>
+        <Image src={props.image} alt={props.title} width={100} height={100} />
+        </Link>
+    </div>
+  );
+};
+
+export default GameLinkItem;
