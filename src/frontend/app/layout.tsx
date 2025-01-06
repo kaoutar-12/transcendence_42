a496df772487c955a345591e4396f67285320f70
hznagui <houdayfahouayfa7@gmail.com>
2025-01-06 19:46:50 +0100

    create a design for the login page

--- src/frontend/app/layout.tsx
+++ src/frontend/app/layout.tsx
@@ -1,24 +1,53 @@
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
+// import { Inter } from "next/font/google";
+// import "./globals.css";
+// import Sidebar from "@/components/Sidebar";
+// const inter = Inter({ subsets: ["latin"] });
+
+// export const metadata: Metadata = {
+//   title: "Ping Pong Game",
+//   description: "Ping Pong with next-js & Django",
+// };
+
+// export default function RootLayout({
+//   children,
+// }: Readonly<{
+//   children: React.ReactNode;
+// }>) {
+//   return (
+//     <html lang="en">
+//       <link rel="icon" type="image/x-icon" href="logo.ico"></link>
+//       <body className={inter.className}>
+//         <Sidebar />
+//         {children}
+//       </body>
+//     </html>
+//   );
+// }
+'use client';
+
+import { usePathname } from 'next/navigation';
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/components/Sidebar";
-const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Ping Pong Game",
-  description: "Ping Pong with next-js & Django",
-};
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
+  const pathname = usePathname();
+  const isAuthPage = pathname?.includes('/auth/');
+
   return (
     <html lang="en">
-      <link rel="icon" type="image/x-icon" href="logo.ico"></link>
+      <head>
+        <link rel="icon" type="image/x-icon" href="logo.ico" />
+      </head>
       <body className={inter.className}>
-        <Sidebar />
+        {!isAuthPage && <Sidebar />}
         {children}
       </body>
     </html>
