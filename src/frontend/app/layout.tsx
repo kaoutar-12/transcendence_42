e1e93286d828b007d2f354c584eb8ff94044deca
hznagui <houdayfahouayfa7@gmail.com>
2025-01-07 15:15:58 +0100

    make the register page works

--- src/frontend/app/layout.tsx
+++ src/frontend/app/layout.tsx
@@ -1,32 +1,3 @@
-// import type { Metadata } from "next";
-// import { Inter } from "next/font/google";
-// import "./globals.css";
-// import Sidebar from "@/components/Sidebar";
-// const inter = Inter({ subsets: ["latin"] });
-
-// export const metadata: Metadata = {
-//   title: "Ping Pong Game",
-//   description: "Ping Pong with next-js & Django",
-// };
-
-// export default function RootLayout({
-//   children,
-// }: Readonly<{
-//   children: React.ReactNode;
-// }>) {
-//   return (
-//     <html lang="en">
-//       <link rel="icon" type="image/x-icon" href="logo.ico"></link>
-//       <body className={inter.className}>
-//         <Sidebar />
-//         {children}
-//       </body>
-//     </html>
-//   );
-// }
-
-
-///////////////////
 'use client';
 
 import { usePathname } from 'next/navigation';
@@ -37,7 +8,7 @@ import Sidebar from "@/components/Sidebar";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
-  children,
+  children
 }: {
   children: React.ReactNode;
 }) {
