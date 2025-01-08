aa54db3a8dbea4054c364e1d28de14efa37a37e8
hznagui <houdayfahouayfa7@gmail.com>
2025-01-08 16:11:07 +0100

    change the color

--- src/frontend/components/LandingAnimation/LandingAnimation.tsx
+++ src/frontend/components/LandingAnimation/LandingAnimation.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LandingAnimation = () => {
   return (
-    <div className="relative w-full h-screen overflow-hidden bg-gradient-to-r from-indigo-500 via-black-500 to-pink-500 animate-gradient-x">
+    <div className="relative w-full h-screen overflow-hidden bg-gradient-to-r from-black via-red-700 to-red-500 animate-gradient-x">
       <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <div className="w-48 h-48 rounded-full bg-white opacity-20 animate-ping"></div>
         <div className="absolute top-0 left-0 w-48 h-48 rounded-full bg-white opacity-20 animate-ping animation-delay-2000"></div>
