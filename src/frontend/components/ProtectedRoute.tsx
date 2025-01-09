4d8458534c3311895e4dedca2f2bf7fbac7218f4
znagui houdaifa <houdayfahouayfa7@gmail.com>
2025-01-09 22:39:59 +0100

    the accept token is working

--- src/frontend/components/ProtectedRoute.tsx
+++ src/frontend/components/ProtectedRoute.tsx
@@ -11,31 +11,41 @@ export default function ProtectedRoute({ children }: { children: React.ReactNode
   const [isVerified, setIsVerified] = useState(false);
 
   useEffect(() => {
-    const token = localStorage.getItem('access_token');
+    // const token = localStorage.getItem('access_token');
     
-    if (!token) {
-      router.push('/auth/login');
-      return;
-    }
+    // if (!token) {
+    //   router.push('/auth/login');
+    //   return;
+    // }
 
     // Verify token on component mount
     const verifyAccess = async () => {
       try {
-        const response = await fetch('http://localhost:8000/api/verify-token/', {
-          method: 'GET',
-            headers: {
-              'Authorization': `Bearer ${token}`
-            }
-          }); 
-          // const response = await api.get('/verify-token/');
+        // const response = await fetch('http://localhost:8000/api/verify-token/', {
+        //   method: 'GET',
+        //     headers: {
+        //       'Authorization': `Bearer ${token}`
+        //     }
+        //   }); 
+        //   // const response = await api.get('/verify-token/');
         
-        if (!response) {
-          localStorage.removeItem('access_token');
-          localStorage.removeItem('refresh_token');
-          router.push('/auth/login');
-        } else {
-          setIsVerified(true);
-        }
+        // if (!response.ok) {
+        //   localStorage.removeItem('access_token');
+        //   localStorage.removeItem('refresh_token');
+        //   router.push('/auth/login');
+        // } else {
+        //   setIsVerified(true);
+        // }
+        const response = await api.get('/verify-token/'); 
+            // const response = await api.get('/verify-token/');
+          
+          if (!response) {
+            localStorage.removeItem('access_token');
+            localStorage.removeItem('refresh_token');
+            router.push('/auth/login');
+          } else {
+            setIsVerified(true);
+          }
       } catch {
         router.push('/auth/login');
       }
