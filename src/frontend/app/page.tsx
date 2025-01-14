778e1adfb5bb089301f2f8643ad0880f57cc801e
yrhiba <yahyarhiba@gmail.com>
2025-01-14 14:11:40 +0100

    Add temporaly home page.
    	modified:   src/frontend/app/page.tsx
    	new file:   src/frontend/styles/home.css

--- src/frontend/app/page.tsx
+++ src/frontend/app/page.tsx
@@ -1,10 +1,17 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Link from "next/link";
+import "@/styles/home.css"
 
 export default function App() {
   return (
-    <main>
-      hhhhhhhhhh
+    <main className="main">
+      <h1 className="title">
+        Welcom To Ft_Trancendence
+      </h1>
+
+      <nav className="nav-bar">
+        <div className="item"><Link href="/auth/login">Login</Link></div>
+        <div className="item"><Link href="/auth/register">Register</Link></div>
+      </nav>
     </main>
   );
 }
