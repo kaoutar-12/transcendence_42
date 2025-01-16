329f88298f838fe5039a6d235b337c8a28c992e7
hznagui <houdayfahouayfa7@gmail.com>
2025-01-16 12:30:04 +0100

    fitch the user data in the setings page

--- src/backend/authentication/serializers.py
+++ src/backend/authentication/serializers.py
@@ -1,16 +1,3 @@
-# from rest_framework import serializers
-# from .models import User
-
-# class UserSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = ( 'username', 'email', 'password')
-#         extra_kwargs = {'password': {'write_only': True}}
-
-#     def create(self, validated_data):
-#         user = User.objects.create_user(**validated_data)
-#         return user
-
 from rest_framework import serializers
 from .models import User
 
@@ -20,7 +7,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'email', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
+        extra_kwargs = {'password': {'write_only': True} }
 
     def create(self, validated_data):
         user = User.objects.create_user(
