632656c900900ae840316c4c41ebf9562d0cfcc9
rghouzra <rghouzra@student.1337.ma>
2025-01-18 23:26:59 +0100

    Refactor queue URL paths to simplify routing

--- src/backend/game/urls.py
+++ src/backend/game/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('queue/join/', views.join_queue, name='join_queue'),
-	path('queue/leave/', views.leave_queue, name='leave_queue'),
+	path('join/', views.join_queue, name='join_queue'),
+	path('leave/', views.leave_queue, name='leave_queue'),
 ]
