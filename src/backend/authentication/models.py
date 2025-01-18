35294aa9227a59a38411c4ce46081eeab7537e07
hznagui <houdayfahouayfa7@gmail.com>
2025-01-18 14:25:12 +0100

    setup the library for profile image, aded the image field at DB

--- src/backend/authentication/models.py
+++ src/backend/authentication/models.py
@@ -4,6 +4,11 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     nickname = models.CharField(max_length=50, blank=True, null=True)
+    profile_image = models.ImageField(
+    upload_to='user_profiles/',
+    blank=True,
+    null=True
+)
     
     class Meta:
         app_label = 'authentication'
