997f30d3c408f8a94ec2f982370fdba739f1b53d
rghouzra <rghouzra@student.1337.ma>
2025-01-24 21:00:29 +0100

    refactor entrypoint script to execute passed commands instead of hardcoding Daphne

--- src/backend/docker-conf/entrypoint.sh
+++ src/backend/docker-conf/entrypoint.sh
@@ -9,5 +9,4 @@ python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py createsuperuser --noinput || true
 
-cd /app
-python3 -m daphne -b 0.0.0.0 -p 8000 backend.asgi:application
+exec "$@"
