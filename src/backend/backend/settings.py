16a205fec9b163cfbe8c7bd6ea65e14979618482
hznagui <houdayfahouayfa7@gmail.com>
2025-01-26 11:05:24 +0100

    update settings

--- src/backend/backend/settings.py
+++ src/backend/backend/settings.py
@@ -142,20 +142,6 @@ CORS_ALLOWED_ORIGINS = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
-# CORS_ALLOW_HEADERS = [
-#     "accept",
-#     "accept-encoding",
-#     "authorization",
-#     "content-type",
-#     "dnt",
-#     "origin",
-#     "user-agent",
-#     "x-csrftoken",
-#     "x-requested-with",
-#     "credentials",
-# ]
-
-
 
 ALLOWED_HOSTS = ["*"]
         # 'rest_framework_simplejwt.authentication.JWTAuthentication',
