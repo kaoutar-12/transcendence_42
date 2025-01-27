789cefba8a5f072731966e3037c0cd5e5dcc7145
yrhiba <yahyarhiba@gmail.com>
2025-01-27 20:31:39 +0100

    Remove the tic-tac-toe game page and add to future games page
    	modified:   src/frontend/app/games/page.tsx
    	deleted:    src/frontend/app/games/tictactoe/page.tsx

--- src/frontend/app/games/page.tsx
+++ src/frontend/app/games/page.tsx
@@ -6,15 +6,15 @@ type Props = {}
 
 const Games = (props: Props) => {
   return (
-    <div className="games-container">
-      <div className="games-header">
-        <h1>List Of Games</h1>
+      <div className="games-container">
+        <div className="games-header">
+          <h1>List Of Games</h1>
+        </div>
+        <div className="games-list">
+          <GameLinkItem title="2D Ping Pong" image="/pinpong-game-icon.png" href="/games/pingpong" status="New" />
+          <GameLinkItem title="Tic Tac Toe" image="/tictac-game-icon.png" href="#" status="For Future"/>
+        </div>
       </div>
-      <div className="games-list">
-        <GameLinkItem title="2D Ping Pong" image="/pinpong-game-icon.png" href="/games/pingpong" status="New" />
-        <GameLinkItem title="Tic Tac Toe" image="/tictac-game-icon.png" href="/games/tictactoe" status="In progress"/>
-      </div>
-    </div>
   )
 }
 
