16571d51850cf77ee39532ebbcc1cdba0b60a6a5
kaoutar-12 <kmouradi@student.1337.ma>
2025-02-01 20:39:10 +0100

    Working on Dashboard

--- src/frontend/app/(dashboard)/home/page.tsx
+++ src/frontend/app/(dashboard)/home/page.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React from "react";
 import "@/styles/dashboard.css";
 
 export default function Home() {
   return (
     <div className="home">
-    <div className="profile-info">
-      <div className="info"></div>
-    </div>
+      <div className="profile-info">
+        <div className="avatar"></div>
+        <div className="info">
+          <div className="friends-list">
+            <span>Friends</span>
+            <span>3</span>
+          </div>
+          <div className="username">Username</div>
+          <div className="online">online</div>
+        </div>
+      </div>
     </div>
   );
 }
