dcd0bae709ef11e99cc59ce0e54074c52e47d40b
kaoutar-12 <kmouradi@student.1337.ma>
2025-02-02 17:45:29 +0100

    Global socket and Chat notification Kinda working

--- src/backend/backend/urls.py
+++ src/backend/backend/urls.py
@@ -18,10 +18,16 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
+from . import consumers
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('authentication.urls')),
-    path('api/game/', include('game.urls'))
+    path('api/game/', include('game.urls')),
+    path('api/', include('chat.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+global_websocket_urlpatterns = [
+    path('ws/global/', consumers.GlobalConsumer.as_asgi()),
+]
