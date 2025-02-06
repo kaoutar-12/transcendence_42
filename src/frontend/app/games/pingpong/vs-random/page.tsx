7e9b5f4302722bd3e4b0f5a4986d39c8a3ffcea6
yrhiba <yahyarhiba@gmail.com>
2025-02-06 15:56:45 +0100

    To be tested by rghouzra
    	new file:   src/frontend/app/games/pingpong/vs-random/page.tsx

---
+++ src/frontend/app/games/pingpong/vs-random/page.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+// components/MatchmakingQueue.tsx
+import { useState, useEffect } from 'react';
+
+
+const MatchmakingQueue = () => {
+    const [status, setStatus] = useState('waiting');
+    const [socket, setSocket] = useState<WebSocket | null>(null);
+
+
+    useEffect(() => {
+        // Connect to matchmaking WebSocket
+        const ws = new WebSocket('ws://localhost:8000/api/ws/queue/');
+        
+        ws.onopen = () => {
+            setStatus('searching');
+        };
+
+        ws.onmessage = (event) => {
+            console.log(event.data);
+        };
+
+        ws.onclose = () => {
+            setStatus('disconnected');
+        };
+
+        setSocket(ws);
+
+        // Cleanup on component unmount
+        return () => {
+            if (ws) {
+                ws.close();
+            }
+        };
+    }, []);
+
+    return (
+        <div className="flex flex-col items-center justify-center">
+            Hello World, Match making
+        </div>
+    );
+};
+
+export default MatchmakingQueue;
