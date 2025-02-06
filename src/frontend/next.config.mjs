502a3c9352a672474ba8fadb17ad39db4d942c1e
hznagui <houdayfahouayfa7@gmail.com>
2025-02-06 14:43:09 +0100

    make the 42 inrta works

--- src/frontend/next.config.mjs
+++ src/frontend/next.config.mjs
@@ -11,6 +11,25 @@ const nextConfig = {
     ],
   },
   reactStrictMode: false,
+  async redirects() {
+    return [
+      // Basic redirect
+      {
+        source: '/about',
+        destination: '/',
+        permanent: true,
+      },
+      // destination: 'https://api.intra.42.fr/oauth/authorize'+'?client_id='+process.env.UID+'&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2Fapi%2Foauth%2F&response_type=code',
+      // Wildcard path matching
+      {
+        source: '/oauth',
+        destination: `https://api.intra.42.fr/oauth/authorize?client_id=${process.env.UID}&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2Fapi%2Foauth%2F&response_type=code`,
+        permanent: true,
+        basePath: false,
+      },
+    ]
+  },
 };
+
 
 export default nextConfig;
