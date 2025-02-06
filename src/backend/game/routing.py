958b18b868282d6299f958a97655aeaea749bbf8
rghouzra <rghouzra@student.1337.ma>
2025-02-06 16:10:43 +0100

    Update websocket routing to use PongGameConsumer for game connections

--- src/backend/game/routing.py
+++ src/backend/game/routing.py
@@ -2,6 +2,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/game/(?P<game_id>\d+)/', consumers.GameConsumer.as_asgi()),
+    re_path(r'ws/game/(?P<game_id>\d+)/', consumers.PongGameConsumer.as_asgi()),
     re_path(r'ws/queue/', consumers.QueueConsumer.as_asgi()),
 ]
