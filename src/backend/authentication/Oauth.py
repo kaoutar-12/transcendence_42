ddc5ad516dd53febd0ad6a929dd678ac37f5755a
hznagui <houdayfahouayfa7@gmail.com>
2025-02-07 19:06:27 +0100

    exchange the code with access token

--- src/backend/authentication/Oauth.py
+++ src/backend/authentication/Oauth.py
@@ -13,6 +13,25 @@ def Oauth(request):
     
     if not code:
         return Response({'message': 'Code is required'})
-    
+    url = "https://api.intra.42.fr/oauth/token/"
+    headers = {
+        "Content-Type": "application/json",
+    }
+    body={
+        'grant_type':'authorization_code',
+        'client_id':'u-s4t2ud-4719e3779c459d1f6e4055ac6167c2cdc82154e41df26314bb0cdca56a1210e1',
+        'client_secret':'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',
+        'code': code,
+        'redirect_uri': 'http://localhost:3000/call/',
+    }
+    try:
+        response = requests.post(url, json=body, headers=headers)        
+        if (response.ok):
+            data = response.json()
+            print(data['access_token'])
+        return Response({'message': 'code recived successfully'})
+    except requests.exceptions.RequestException as e:
+        return Response({"error": str(e)})
+
     print(code)
     return Response({'message': 'code recived successfully'})
