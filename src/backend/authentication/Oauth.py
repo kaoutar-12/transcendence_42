5869bf55514e6f82500bc3913957ea722e7d05fd
hznagui <houdayfahouayfa7@gmail.com>
2025-02-08 17:25:22 +0100

    register in 42 is now working

--- src/backend/authentication/Oauth.py
+++ src/backend/authentication/Oauth.py
@@ -1,6 +1,16 @@
 from rest_framework.permissions import AllowAny
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken 
+from .serializers import UserSerializer,UserProfileImageSerializer
+from django.conf import settings
+from rest_framework import status
+import secrets
+
+
+
+from .models import TwoFactorAuth,User
+
 import requests
 
 
@@ -32,11 +42,69 @@ def Oauth(request):
             headers["Authorization"] = f"Bearer {data['access_token']}"
             response1 = requests.get(url1, headers=headers) 
             if (response1.ok):
-                print(data['access_token'])
+                data = response1.json()
+                try:
+                    user = User.objects.get(email=data["email"])
+                    # login method 
+                
+                    serialzer = UserSerializer(user.data)
+                  
+                    refresh = RefreshToken.for_user(user)
+                    response = Response({
+                        'message':"User entered Successfully;"
+                    })
+                    response.set_cookie(
+                        'access_token',
+                        refresh.access_token,
+                        httponly=True,
+                        # secure=True,
+                        samesite='Strict',
+                        max_age=settings.SIMPLE_JWT.get('ACCESS_TOKEN_LIFETIME').total_seconds()
+                        )
+                    response.set_cookie(
+                        'refresh_token',
+                        refresh,
+                        httponly=True,
+                        # secure=True,
+                        samesite='Strict',
+                        max_age=settings.SIMPLE_JWT.get('REFRESH_TOKEN_LIFETIME').total_seconds()
+                        )
+                    return response
+                except User.DoesNotExist:
+                    # register method 
+                    transformed_data = {
+                        'username': data['first_name'],
+                        'email': data['email'],
+                        'nickname': data['login'],
+                        'password': secrets.token_urlsafe(32)
+                    }
+                    serializer = UserSerializer(data=transformed_data, context={'is_42_user': True})
+                    if serializer.is_valid():
+                        user = serializer.save()
+                        refresh = RefreshToken.for_user(user)
+                        response = Response({'message':"User entered Successfully;"}, status=status.HTTP_201_CREATED)
+                        response.set_cookie(
+                            'access_token',
+                            refresh.access_token,
+                            httponly=True,
+                            # secure=True,
+                            samesite='Strict',
+                            max_age=settings.SIMPLE_JWT.get('ACCESS_TOKEN_LIFETIME').total_seconds()
+                            )
+                        response.set_cookie(
+                            'refresh_token',
+                            refresh,
+                            httponly=True,
+                            # secure=True,
+                            samesite='Strict',
+                            max_age=settings.SIMPLE_JWT.get('REFRESH_TOKEN_LIFETIME').total_seconds()
+                            )
+                        return response
+                    return Response(serializer.errors)
+                    
         
         return Response({'message': 'code recived successfully'})
     except requests.exceptions.RequestException as e:
         return Response({"error": str(e)})
-
     print(code)
     return Response({'message': 'code recived successfully'})
