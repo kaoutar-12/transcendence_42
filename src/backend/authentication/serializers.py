55a25bf38a14935efffd501ef65869fab96919a3
hznagui <houdayfahouayfa7@gmail.com>
2025-02-09 16:29:01 +0100

    now the image is imported with 42 user

--- src/backend/authentication/serializers.py
+++ src/backend/authentication/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import User ,TwoFactorAuth
+import requests
+from django.core.files.base import ContentFile
 
 
 class UserBasicSerializer(serializers.ModelSerializer):
@@ -12,10 +14,12 @@ class UserSerializer(serializers.ModelSerializer):
     twoFactorEnabled = serializers.SerializerMethodField()
     friends = serializers.SerializerMethodField()
     blocked_users = serializers.SerializerMethodField()
+    image_url = serializers.URLField(write_only=True, required=False)
+
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'password','nickname','profile_image','twoFactorEnabled'
+        fields = ('id', 'username', 'email', 'password','nickname','profile_image','twoFactorEnabled', 'image_url'
                   ,'friends','blocked_users')
         extra_kwargs = {'password': {'write_only': True},
                         'nickname': {'required': False},
@@ -33,12 +37,27 @@ class UserSerializer(serializers.ModelSerializer):
         return user
     
     def create_42_user(self, validated_data):
+        image_url = validated_data.pop('image_url', None)
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password'],
             nickname=validated_data['nickname'],
         )
+        if image_url:
+            try:
+                response = requests.get(image_url)
+                if response.status_code == 200:
+                    image_name = image_url.split('/')[-1]
+                    user.profile_image.save(
+                        image_name,
+                        ContentFile(response.content),
+                        save=True
+                    )
+            except Exception as e:
+                print(f"Error downloading image: {e}")
+        
+        
         return user
     
     def get_twoFactorEnabled(self, obj):
