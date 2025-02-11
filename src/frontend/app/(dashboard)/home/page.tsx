52aa05272e388e5703ededd20d77344c86435127
kaoutar-12 <kmouradi@student.1337.ma>
2025-02-11 23:25:37 +0100

    done with blocking system / just one minor bug / finished chat ui / working on dashboard ui

--- src/frontend/app/(dashboard)/home/page.tsx
+++ src/frontend/app/(dashboard)/home/page.tsx
@@ -1,23 +1,56 @@
 import React from "react";
 import "@/styles/dashboard.css";
+import LevelBar from "@/components/ProcessBar";
+import MatchHistory from "@/components/HistoryTable";
+
+interface MatchHistoryItem {
+  component: string;
+  level: number;
+  result: string;
+  score: string;
+  date: string;
+}
+
+const sampleMatches: MatchHistoryItem[] = [
+  { component: "Player", level: 2, result: "WIN", score: "5 - 2", date: "09/09/2024" },
+  { component: "Player", level: 2, result: "LOSE", score: "5 - 2", date: "09/09/2024" },
+  { component: "Player", level: 2, result: "LOSE", score: "5 - 2", date: "09/09/2024" },
+  { component: "Player", level: 2, result: "LOSE", score: "5 - 2", date: "09/09/2024" },
+  { component: "Player", level: 2, result: "LOSE", score: "5 - 2", date: "09/09/2024" },
+  { component: "Player", level: 2, result: "LOSE", score: "5 - 2", date: "09/09/2024" },
+  { component: "Player", level: 2, result: "LOSE", score: "5 - 2", date: "09/09/2024" },
+  { component: "Player", level: 2, result: "LOSE", score: "5 - 2", date: "09/09/2024" },
+];
 
 export default function Home() {
   return (
     <div className="home">
-      <div className="profile-info">
-        <div className="avatar"></div>
-        <div className="info">
-          <div className="friends-list">
-            <div className="friends">Friends</div>
-            <div>3</div>
-          </div>
-          <div className="username">Username</div>
-          <div className="online">
-            <span>Online</span>
-            <div className="online-col"></div>
+      <section>
+        <div className="profile-info">
+          <div className="avatar"></div>
+          <div className="info">
+            <div className="friends-list">
+              <div className="friends">Friends</div>
+              <div>3</div>
+            </div>
+            <div className="username">Username</div>
+            <div className="online">
+              <span>Online</span>
+              <div className="online-col"></div>
+            </div>
           </div>
         </div>
-      </div>
+        <LevelBar level={4} percentage={30} />
+      </section>
+      <section>
+        <div className="grid-container">
+          <div className="item-1">1</div>
+          <div className="item-2">2</div>
+          <div className="item-3">
+            <MatchHistory matches={sampleMatches} />
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
