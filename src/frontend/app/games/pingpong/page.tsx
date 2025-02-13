1aba78839e9455aaf090551516b291300783fe31
yrhiba <yahyarhiba@gmail.com>
2025-02-13 02:16:24 +0100

    Add 2 vs 2 local pingpong 2d game mode.
    	new file:   src/frontend/app/games/pingpong/2-vs-2-local/page.tsx
    	modified:   src/frontend/app/games/pingpong/page.tsx
    	new file:   src/frontend/public/2vs2.jpeg

--- src/frontend/app/games/pingpong/page.tsx
+++ src/frontend/app/games/pingpong/page.tsx
@@ -11,10 +11,10 @@ const PingPong = (props: Props) => {
         <h1>Tic Tac Toe | Chosse a Game Mode </h1>
       </div>
       <div className="games-list">
-        <GameLinkItem href="/games/pingpong/1-vs-1-local" title="Local 1 vs 1" image="/1vs1.png" status="In progress" />
+        <GameLinkItem href="/games/pingpong/1-vs-1-local" title="Local 1 vs 1" image="/1vs1.png" status="Working" />
+        <GameLinkItem href="/games/pingpong/2-vs-2-local" title="Local 2 vs 2" image="/2vs2.jpeg" status="Working" />
         <GameLinkItem href="/games/pingpong/local-tournament" title="Local Tournament" image="/tournament.png" status="In progress" />
         <GameLinkItem href="/games/pingpong/vs-random" title="VS Random" image="/vs-random.png" status="In progress" />
-        <GameLinkItem href="#" title="VS IA" image="/vs-ia.png" status="For future" />
       </div>
     </div>
   )
