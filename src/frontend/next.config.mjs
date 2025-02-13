ea0beebefd87cabc176ed7523999225361bb3493
znagui houdaifa <houdayfahouayfa7@gmail.com>
2025-02-13 01:53:48 +0100

    clean the code and move some urls to env

--- src/frontend/next.config.mjs
+++ src/frontend/next.config.mjs
@@ -18,17 +18,8 @@ const nextConfig = {
   reactStrictMode: false,
   async redirects() {
     return [
-      // Basic redirect
-      {
-        source: '/about',
-        destination: '/',
-        permanent: true,
-      },
-      // destination: 'https://api.intra.42.fr/oauth/authorize'+'?client_id='+process.env.UID+'&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2Fapi%2Foauth%2F&response_type=code',
-      // Wildcard path matching
       {
         source: '/oauth1',
-        // destination: `https://api.intra.42.fr/oauth/authorize?client_id=${process.env.UID}&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2Fapi%2Foauth%2F&response_type=code`,
         destination: `https://api.intra.42.fr/oauth/authorize?client_id=${process.env.UID}&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fcall%2F&response_type=code`,
         permanent: true,
         basePath: false,
