e35e54abc7e00551b61ab3c23a1409f6dda68a9b
hznagui <houdayfahouayfa7@gmail.com>
2025-02-20 14:32:47 +0100

    make a new version of the api that simple, work better

--- src/frontend/app/utils/api.ts
+++ src/frontend/app/utils/api.ts
@@ -1,68 +1,109 @@
+// import axios from 'axios';
+// import Router from 'next/router';
+
+// const api = axios.create({
+//     baseURL: `${process.env.NEXT_PUBLIC_API_URL}`,
+//     withCredentials: true
+// });
+
+// // Single flag to track refresh state
+// let isRefreshing = false;
+// // Store pending requests
+// let failedQueue: { resolve: (value: unknown) => void; reject: (reason?: any) => void; }[] = [];
+
+
+// const processQueue = (error: any, token = null) => {
+//     failedQueue.forEach(prom => {
+//         if (error) {
+//             prom.reject(error);
+//         } else {
+//             prom.resolve(token);
+//         }
+//     });
+//     failedQueue = [];
+// };
+
+// api.interceptors.request.use(
+//     (config) => {
+//         return config;
+//     },
+//     (error) => Promise.reject(error)
+// );
+
+// api.interceptors.response.use(
+//     (response) => response,
+//     async (error) => {
+//         const originalRequest = error.config;
+
+//         if (error.response?.status === 401 && !originalRequest._retry) {
+//             if (isRefreshing) {
+//                 // If already refreshing, queue this request
+//                 return new Promise((resolve, reject) => {
+//                     failedQueue.push({ resolve, reject });
+//                 }).then(token => {
+//                     originalRequest.headers.credentials = 'include';
+//                     return api(originalRequest);
+//                 });
+//             }
+
+//             isRefreshing = true;
+//             originalRequest._retry = true;
+//             try {
+//                 const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/token/refresh/`, {},{withCredentials: true}
+//             );
+//                 isRefreshing = false;
+//                 return api(originalRequest);
+//             } catch (err) {
+//                 // Clear queue with error
+//                 processQueue(err, null);
+//                 isRefreshing = false;
+//                 Router.push('/login');
+//                 return Promise.reject(err);
+//             }
+//         }
+//         return Promise.reject(error);
+//     }
+// );
+
+// export default api;
+
+
 import axios from 'axios';
 import Router from 'next/router';
 
-const api = axios.create({
-    baseURL: `${process.env.NEXT_PUBLIC_API_URL}`,
-    withCredentials: true
-});
-
-// Single flag to track refresh state
-let isRefreshing = false;
-// Store pending requests
-let failedQueue: { resolve: (value: unknown) => void; reject: (reason?: any) => void; }[] = [];
-
-
-const processQueue = (error: any, token = null) => {
-    failedQueue.forEach(prom => {
-        if (error) {
-            prom.reject(error);
-        } else {
-            prom.resolve(token);
-        }
-    });
-    failedQueue = [];
-};
-
-api.interceptors.request.use(
-    (config) => {
-        return config;
-    },
-    (error) => Promise.reject(error)
-);
+const api=axios.create({baseURL:process.env.NEXT_PUBLIC_API_URL, withCredentials: true});
+let isRefreshing=false;
 
 api.interceptors.response.use(
-    (response) => response,
-    async (error) => {
-        const originalRequest = error.config;
 
-        if (error.response?.status === 401 && !originalRequest._retry) {
-            if (isRefreshing) {
-                // If already refreshing, queue this request
-                return new Promise((resolve, reject) => {
-                    failedQueue.push({ resolve, reject });
-                }).then(token => {
-                    originalRequest.headers.credentials = 'include';
-                    return api(originalRequest);
-                });
-            }
+    (response)=>response,
 
-            isRefreshing = true;
-            originalRequest._retry = true;
-            try {
-                const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/token/refresh/`, {},{withCredentials: true}
-            );
-                isRefreshing = false;
-                return api(originalRequest);
-            } catch (err) {
-                // Clear queue with error
-                processQueue(err, null);
-                isRefreshing = false;
-                Router.push('/login');
-                return Promise.reject(err);
-            }
+    async(error)=>
+        {
+            const failedResquest = error.config;
+
+            if (error.response?.status === 401 && !failedResquest._retry)
+                {
+                    failedResquest._retry=true;
+                    if (!isRefreshing)
+                        {
+                            try
+                            {
+                                isRefreshing=true;
+                                await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/token/refresh/`,{},{withCredentials:true});
+                                isRefreshing=false;
+                                return api(failedResquest);
+                            }
+                            catch(error)
+                            {
+                                isRefreshing=true;
+                                Router.push('/login');
+                                return Promise.reject(error);
+                            }
+                        }
+                }
+                return Promise.reject(error);
         }
-        return Promise.reject(error);
-    }
-);
 
+);
 export default api;
