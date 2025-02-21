1cff5b26e276fe61d3e3910e6b767cd04a695fd6
rghouzra <rghouzra@student.1337.ma>
2025-02-21 17:06:33 +0100

    feat: add endpoint to retrieve winrate for a user in MatchHistory

--- src/backend/game/urls.py
+++ src/backend/game/urls.py
@@ -4,4 +4,5 @@ from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
   path('/history/<int:user_id>', views.MatchHistoryViewSet.as_view({'get': 'list'})),
+  path('/history/<int:user_id>/winrate', views.MatchHistoryViewSet.as_view({'get': 'winrate'})),
 ]
