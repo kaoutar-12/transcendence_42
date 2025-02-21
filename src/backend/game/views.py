6b13fb4faf4604c78b86278e8985f9612b3da1ef
rghouzra <rghouzra@student.1337.ma>
2025-02-21 17:06:38 +0100

    feat: add winrate action to MatchHistoryViewSet

--- src/backend/game/views.py
+++ src/backend/game/views.py
@@ -1,6 +1,9 @@
 from .models import MatchHistory
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.db import models
 from authentication.models import User
 # Create your views here.
 
@@ -12,3 +15,10 @@ class MatchHistoryViewSet(viewsets.ReadOnlyModelViewSet):
 			return MatchHistory.objects.filter(
 				models.Q(player1=user_id) | models.Q(player2=user_id)
 			)
+
+	@action(detail=False, methods=['get'])
+	def winrate(self, request, user_id=None):
+		if user_id:
+			winrate = MatchHistory.get_winrate(user_id)
+			return Response({'winrate': winrate})
+		return Response({'error': 'User ID is required'}, status=400)
