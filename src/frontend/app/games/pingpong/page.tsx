0d4fbe335f90b6f51bf9ddb0aa97c5cfa03774a0
yrhiba <yahyarhiba@gmail.com>
2025-02-25 00:52:21 +0000

    modified:   src/frontend/app/games/pingpong/page.tsx

--- src/frontend/app/games/pingpong/page.tsx
+++ src/frontend/app/games/pingpong/page.tsx
@@ -13,8 +13,8 @@ const PingPong = (props: Props) => {
       <div className="games-list">
         <GameLinkItem href="/games/pingpong/1-vs-1-local" title="Local 1 vs 1" image="/1vs1.png" status="Working" />
         <GameLinkItem href="/games/pingpong/2-vs-2-local" title="Local 2 vs 2" image="/2vs2.jpeg" status="Working" />
-        <GameLinkItem href="/games/pingpong/local-tournament" title="Local Tournament" image="/tournament.png" status="In progress" />
-        <GameLinkItem href="/games/pingpong/vs-random" title="VS Random" image="/vs-random.png" status="In progress" />
+        <GameLinkItem href="/games/pingpong/local-tournament" title="Local Tournament" image="/tournament.png" status="Working" />
+        <GameLinkItem href="/games/pingpong/vs-random" title="VS Random" image="/vs-random.png" status="In Progress" />
       </div>
     </div>
   )
